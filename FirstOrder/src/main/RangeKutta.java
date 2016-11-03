e6553d9539a763fdefc79ff28050394dfd0f01c0
madhuriSo <madhuri.padule@gmail.com>
2016-11-03 10:58:40 -0400

    Updated method

--- FirstOrder/src/main/RangeKutta.java
+++ FirstOrder/src/main/RangeKutta.java
@@ -4,12 +4,16 @@ package main;
 public class RangeKutta {
 
     // The number of steps to use in the interval
-    public static final int N = 2;
+    public static final double N = 2;
 
 
     // The derivative dx/dt at a given value of t and x.
     public static double deriv(double t, double x)
     {
+        double a,b,c;
+        a=Math.exp(-t);
+        b=0.4*x;
+        c=((3*(Math.exp(-t)))-(0.4*x));
         return (3*(Math.exp(-t))-(0.4*x));
     }
 
@@ -18,31 +22,14 @@ public class RangeKutta {
     public static void main(String[] argv)
     {
         // `h' is the size of each step.
-        double h = 1.0 / N;
+        double h = 1.5;
         double A, B, C, D;
         double t, x;
         int i;
 
 
-        // Computation by Euclid's method
-        // Initialize x
-        x = 5;
-
-        for (i=0; i<N; i++)
-        {
-            // Step through, updating t and incrementing x
-            t = i * h;
-
-            x += h * deriv(t, x);
-        }
-
-        // Print out the result that we get.
-        System.out.println("Using the Euler method "
-                + "The value at t=1 is:");
-        System.out.println(x);
-
-
         // Computation by 4th order Runge-Kutta
+
         // Initialize x
         x = 5;
 
@@ -58,25 +45,19 @@ public class RangeKutta {
             D = h * deriv(t + h, x + C);
 
             // Incrementing x
-          //  x += A/6 + B/3+ C/3 + D/6;
+           x += A/6 + B/3+ C/3 + D/6;
+            System.out.println(" Value of X: "+x+" at t= "+i);
 
 
-            x=x+(h*(A+(2*B)+(2*C)+D)/6);
         }
 
         // Print out the result that we get.
-        System.out.println();
+
         System.out.println("Using 4th order Runge-Kutta "
-                + "The value at t=1 is:");
+                + "The value at t=3 is:");
         System.out.println(x);
 
 
-        // Computation by closed form solution
-        // Print out the result that we get.
-        System.out.println();
-        System.out.println("The value really is:");
-        x = (Math.exp(0.5) - Math.exp(-0.5)) / 2;
-        System.out.println(x);
     }
 }
 
