753ad16319f0640dc70f37ef0bdf0d7a2c5e6aad
HefeiQiu <qiuhefei@gmail.com>
2017-02-01 20:50:14 -0500

    fixed getAlphaPlusBeta, getValue, getGammaCurrent, getGammaNext, calling of getValue method when calculating B,C,D

--- NewImplementation/src/EquationSloshingMatrix.java
+++ NewImplementation/src/EquationSloshingMatrix.java
@@ -4,18 +4,18 @@ public class EquationSloshingMatrix {
     double g=9.81;
 
     //Parameters of liquid
-    int RHO=1000;
+    double RHO=1000.0;
     double MU=0.00152;
     double h=0.05;
     double ZETA=0.07;
 
     //Parameters of liquid with respect to time
-    int T_MAX=8;
+    int T_MAX=20;
     double DELTA_T=1.0/256;
 
     //Parameter of the tank
     double L=1.0;
-    int N=100;
+    int N=50;
     double l=(L/N);
 
     //Parameters for external vibrations
@@ -43,7 +43,19 @@ public class EquationSloshingMatrix {
     public double getAlphaPlusBeta (double eta,double x0_dot, double x1_dot, double x0,double x1) {
     	double alpha, beta;
         alpha=0.5*  RHO * g *((eta*eta)+(2*eta*h));
-        beta=((1/3) *RHO*(eta+h)*(eta+h)*(2*h*l*(x1_dot-x0_dot)*(x1_dot-x0_dot))/((x1-x0+l)*(x1-x0+l)*(x1-x0+l)));
+        beta=((1.0/3) *RHO*(eta+h)*(eta+h)*(2*h*l*(x1_dot-x0_dot)*(x1_dot-x0_dot))/((x1-x0+l)*(x1-x0+l)*(x1-x0+l)));
+        return alpha + beta;
+    }
+    
+	
+	// temp method for debugging
+    public double getAlphaPlusBetaTemp (double eta,double x0_dot, double x1_dot, double x0,double x1) {
+    	double alpha, beta;
+    	System.out.println("in getAlphaBeta: " + x0_dot + x1_dot);
+        alpha=0.5*  RHO * g *((eta*eta)+(2*eta*h));
+        beta=((1.0/3) *RHO*(eta+h)*(eta+h)*(2*h*l*(x1_dot-x0_dot)*(x1_dot-x0_dot))/((x1-x0+l)*(x1-x0+l)*(x1-x0+l)));
+        System.out.println("alpha: " + alpha);
+        System.out.println("beta: " + beta);
         return alpha + beta;
     }
     
@@ -70,9 +82,37 @@ public class EquationSloshingMatrix {
         double x2=previousX[6],x2_dot=previousX[7],x2_ddot=previousX[8];
         ETA=getEta(x1,x0);
         alphaPlusBeta1=getAlphaPlusBeta(ETA,x0_dot,x1_dot,x0,x1);
+        ETA=getEta(x2,x1); 
         alphaPlusBeta2=getAlphaPlusBeta(ETA,x1_dot,x2_dot,x1,x2);
         fe=getFe(t);
-        f=(alphaPlusBeta1 - alphaPlusBeta2) - (((2 * cc) + cb) * x1_dot) + (cc * x2_dot) - (kb * x1) + fe;
+        f=(alphaPlusBeta1 - alphaPlusBeta2) + cc * x0_dot - (((2 * cc) + cb) * x1_dot) + (cc * x2_dot) - (kb * x1) + fe;
+        return f;
+	}
+	
+	// temp method for debugging
+	public double getValueTemp(double t, double[] previousX) {
+		double f = 0 ,alphaPlusBeta1 = 0, alphaPlusBeta2=0,fe;
+        double x0=previousX[0],x0_dot=previousX[1],x0_ddot=previousX[2];
+        double x1=previousX[3],x1_dot=previousX[4],x1_ddot=previousX[5];
+        double x2=previousX[6],x2_dot=previousX[7],x2_ddot=previousX[8];
+        ETA=getEta(x1,x0);
+        System.out.println("x0_dot_x1_dot: " + x0_dot + "   " + x1_dot);
+        System.out.println("x1_dot_x2_dot: " + x1_dot + "   " + x2_dot);
+        alphaPlusBeta1=getAlphaPlusBetaTemp(ETA,x0_dot,x1_dot,x0,x1);
+        ETA=getEta(x2,x1);
+        alphaPlusBeta2=getAlphaPlusBetaTemp(ETA,x1_dot,x2_dot,x1,x2);
+        fe=getFe(t);
+        f=(alphaPlusBeta1 - alphaPlusBeta2) + cc * x0_dot - (((2 * cc) + cb) * x1_dot) + (cc * x2_dot) - (kb * x1) + fe;
+        
+        System.out.println("ETA: " + ETA);
+        System.out.println("alphaPlusBeta1: " + alphaPlusBeta1);
+		System.out.println("alphaPlusBeta2: " + alphaPlusBeta2);
+		System.out.println("cc * v1[i - 1]: " + cc * x0_dot);
+		System.out.println("(cb + 2d * cc) * v1[i]: " + ((2 * cc) + cb) * x1_dot);
+		System.out.println("cc * v1[i + 1]: " + cc * x2_dot);
+		System.out.println("kb * x1[i]: " + kb * x1);
+		System.out.println("w: " + OMEGA);
+		System.out.println("m * a * w * w * Math.sin(w * dt * td): " + fe);
         return f;
 	}
 	
@@ -82,7 +122,8 @@ public class EquationSloshingMatrix {
         double x1=previousX[3],x1_dot=previousX[4],x1_ddot=previousX[5];
         double x2=previousX[6],x2_dot=previousX[7],x2_ddot=previousX[8];
         ETA=getEta(x1,x0);
-        return (1/3)*RHO*h*l*((ETA+h)*(ETA+h))*(x1_ddot-x0_ddot)/((x1-x0+l)*(x1-x0+l));
+        //return (1.0/3)*RHO*h*l*((ETA+h)*(ETA+h))*(x1_ddot-x0_ddot)/((x1-x0+l)*(x1-x0+l));
+        return (1.0/3)*RHO*h*l*((ETA+h)*(ETA+h))/((x1-x0+l)*(x1-x0+l));
 	}
 	
 	// Calculate Gamma i+1
@@ -91,7 +132,16 @@ public class EquationSloshingMatrix {
 	        double x1=previousX[3],x1_dot=previousX[4],x1_ddot=previousX[5];
 	        double x2=previousX[6],x2_dot=previousX[7],x2_ddot=previousX[8];
 	        ETA=getEta(x2,x1);
-	        return (1/3)*RHO*h*l*((ETA+h)*(ETA+h))*(x2_ddot-x1_ddot)/((x2-x1+l)*(x2-x1+l));
+	        //System.out.println("l: " + l);
+	        //System.out.println("x2: " + x2 + ", x1: " + x1);
+	        //System.out.println("eta: " + ETA);
+	        //System.out.println("eta + h : " + (ETA + h));
+	        //return (1.0/3)*RHO*h*l*((ETA+h)*(ETA+h))*(x2_ddot-x1_ddot)/((x2-x1+l)*(x2-x1+l));
+	       double gamma = (1.0/3)*RHO*h*l*((ETA+h)*(ETA+h))/((x2-x1+l)*(x2-x1+l));
+	       //System.out.println("rho * h * l : " + RHO * h * l);
+	      
+	       //System.out.println("gamma: " + gamma);
+	       return gamma;
 		}
 	
 	
